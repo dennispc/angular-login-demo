1b0bafadd9bf12029edecfdc73db69f106fcb94c
Lars Juul <ljb@easv.dk>
2021-10-29 08:44:22 +0200

    Adding login no backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module')
+      .then(m => m.AuthModule)
+  },
+  {
     path: 'pets',
     loadChildren: () => import('./admin-pets/admin-pets.module')
       .then(m => m.AdminPetsModule)
